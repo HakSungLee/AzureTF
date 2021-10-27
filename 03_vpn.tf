e4b7ce8b877867379ae9c2aab090c071cce0f0a7
HakSungLee <33836667+HakSungLee@users.noreply.github.com>
2021-10-27 21:33:59 +0900

    Update 03_vpn.tf

--- 03_vpn.tf
+++ 03_vpn.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "hem-vpn" {
   name                = "hem-network"
   resource_group_name = azurerm_resource_group.hem_RG.name
-  location            = azurerm_resource_group.hem_RG.location
+  location            = azurerm_resource_group.hem_RG.location // what is this for?
   address_space       = ["10.0.0.0/16"]
 }#setting vpn
